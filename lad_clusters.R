82e0d2741616dec4616cbe73f9300af31d9cf9d4
jwhandley <35394058+jwhandley@users.noreply.github.com>
2023-03-24 12:43:44 +0000

    Fix formatting

--- lad_clusters.R
+++ lad_clusters.R
@@ -6,7 +6,8 @@ library(mclust)
 
 parse_sheet <- function(file_path, sheet_name) {
   # Read the sheet
-  sheet_data <- suppressMessages(read_excel(file_path, sheet = sheet_name, col_names = FALSE))
+  sheet_data <-
+    suppressMessages(read_excel(file_path, sheet = sheet_name, col_names = FALSE))
   
   # Find the row index where the first column entry is "Area Code"
   start_row <- which(sheet_data[[1]] == "Area Code")
@@ -17,13 +18,18 @@ parse_sheet <- function(file_path, sheet_name) {
   }
   
   # Read the data from the sheet, skipping the metadata rows
-  data_frame <- suppressMessages(read_excel(file_path, sheet = sheet_name, skip = start_row - 1))
+  data_frame <-
+    suppressMessages(read_excel(file_path, sheet = sheet_name, skip = start_row - 1))
   
   if (!any(c("Local Authority District", "County or Unitary Authority") %in% names(data_frame))) {
     return(NULL)
   }
   
-  data_frame <- filter(data_frame, !all(is.na(`Local Authority District`),is.na(`County or Unitary Authority`)))
+  data_frame <-
+    filter(data_frame,!all(
+      is.na(`Local Authority District`),
+      is.na(`County or Unitary Authority`)
+    ))
   
   return(data_frame)
 }
@@ -34,7 +40,9 @@ read_excel_sheets_auto <- function(file_path) {
   sheet_names <- excel_sheets(file_path)
   
   # Apply parse_sheet to all sheets in excel file
-  data_frames <- lapply(sheet_names, function(sheet_name) parse_sheet(file_path, sheet_name))
+  data_frames <-
+    lapply(sheet_names, function(sheet_name)
+      parse_sheet(file_path, sheet_name))
   
   # Remove entries with null result
   data_frames <- Filter(Negate(is.null), data_frames)
@@ -47,29 +55,39 @@ sheets <- read_excel_sheets_auto("data/humanreadable.xlsx")
 
 merged_data <- Reduce(function(df1, df2) {
   # Find duplicated column names (excluding 'Area Code')
-  duplicated_columns <- setdiff(intersect(names(df1), names(df2)), "Area Code")
+  duplicated_columns <-
+    setdiff(intersect(names(df1), names(df2)), "Area Code")
   
   # Drop duplicated columns from the second data frame
-  df2 <- df2[ , !(names(df2) %in% duplicated_columns)]
+  df2 <- df2[,!(names(df2) %in% duplicated_columns)]
   
   # Perform inner join
   left_join(df1, df2, by = "Area Code")
 }, sheets)
 
 merged_data %>%
-  select(where(~!all(is.na(.)))) %>%
-  select(-c("Lower 95% Confidence Interval","Upper 95% Confidence Interval","Data accuracy","ITL Level 1")) %>%
-  mutate(across(c(everything(),-c("Area Code","Local Authority District")),~as.numeric(.x)))-> clean_data
+  select(where( ~ !all(is.na(.)))) %>%
+  select(
+    -c(
+      "Lower 95% Confidence Interval",
+      "Upper 95% Confidence Interval",
+      "Data accuracy",
+      "ITL Level 1"
+    )
+  ) %>%
+  mutate(across(c(
+    everything(), -c("Area Code", "Local Authority District")
+  ),  ~ as.numeric(.x))) -> clean_data
 
 glimpse(clean_data)
 
 # Scale numeric data to mean zero, unit variance
-clean_data[,4:40] <- scale(clean_data[,4:40])
+clean_data[, 4:40] <- scale(clean_data[, 4:40])
 # Coerce cleaned data to matrix
-data_mat <- clean_data[,4:40] %>% as.matrix
+data_mat <- clean_data[, 4:40] %>% as.matrix
 
 # Fit probabilistic PCA (to deal with missing data)
-pca <- ppca(data_mat,nPcs=10)
+pca <- ppca(data_mat, nPcs = 10)
 
 # Save loadings to csv
 loadings(pca) %>%
@@ -77,26 +95,30 @@ loadings(pca) %>%
   write_csv("outputs/pca_loadings.csv")
 
 # Fit clusters
-cluster <- Mclust(scores(pca),G=1:10)
+cluster <- Mclust(scores(pca), G = 1:10)
 clean_data$cluster <- cluster$classification
 # cluster <- kmeans(scores(pca),centers=6)
 # clean_data$cluster <- cluster$cluster
 
 # Put scores in cleaned data
-clean_data[,paste("dim",1:10,sep="")] <- scores(pca)
+clean_data[, paste("dim", 1:10, sep = "")] <- scores(pca)
 
 boundaries <- read_sf("boundaries/LAD_DEC_2022_UK_BGC.shp")
 boundaries %>%
-  left_join(clean_data,by=join_by(LAD22CD==`Area Code`)) -> geo_data
+  left_join(clean_data, by = join_by(LAD22CD == `Area Code`)) -> geo_data
 
 geo_data %>%
   filter(!is.na(cluster)) %>%
   ggplot(aes(fill = factor(cluster))) +
   geom_sf() +
   ggthemes::theme_map() +
-  labs(fill = "Cluster",
-       title = "Cluster analysis of local authorities in the UK",
-       subtitle = "ONS subnational indicators dataset",
-       caption = "@jwhandley17")
+  labs(
+    fill = "Cluster",
+    title = "Cluster analysis of local authorities in the UK",
+    subtitle = "ONS subnational indicators dataset",
+    caption = "@jwhandley17"
+  )
 
-ggsave("outputs/lad_clusters.png",width=10,height=16)
+ggsave("outputs/lad_clusters.png",
+       width = 10,
+       height = 16)
